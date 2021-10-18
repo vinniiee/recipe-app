ea44eb1b92d7a9871ce805e258efa1c0d6055458
vinniiee <vs28245@gmail.com>
2021-10-18 23:48:11 +0530

    Handle NumberFormatException. closes #29.

--- src/main/java/com/springframework/recipeapp/controllers/RecipeController.java
+++ src/main/java/com/springframework/recipeapp/controllers/RecipeController.java
@@ -57,9 +57,18 @@ public class RecipeController {
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(NotFoundException.class)
-    public ModelAndView notFoundPage(Exception exception){
+    public ModelAndView handleNotFound(Exception exception){
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("404error");
+        modelAndView.addObject("exception",exception);
+        return modelAndView;
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(NumberFormatException.class)
+    public ModelAndView handleNumberFormat(Exception exception){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("400error");
         modelAndView.addObject("exception",exception);
         return modelAndView;
     }
